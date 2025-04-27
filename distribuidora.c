c40f33314b0e13573a3476fbe2b3a4ba232515a4
camibvir <virginiabaigorria005@gmail.com>
2025-04-27 13:03:03 -0300

    Punto 2 y 3

--- distribuidora.c
+++ distribuidora.c
@@ -14,16 +14,66 @@ typedef struct Nodo{
 
 Nodo * crearListaVacia();
 Nodo * crearNodo(Tarea nuevaTarea);
-void pedirTarea(Nodo **listatareasPendientes);
-void insertarNodoAlFinal(Nodo **listTareasPendientes, Tarea nuevaTarea);
+void pedirTarea(Nodo **start);
+void insertarNodoAlFinal(Nodo **start, Tarea nuevaTarea);
+int buscarNodo (Nodo **start, int id, Nodo **startListaNueva);
+void eliminarNodo (Nodo **start, int id);
+void mostrarLista(Nodo **start);
 void liberarLista(Nodo *lista);
+int buscarPorId(Nodo **start, Nodo **startListaNueva, int idBuscar);
+int buscarPorPalabra(Nodo **start, Nodo **startListaNueva, char palabra[100]);
+
 
 
 int main (){
-    Nodo *listaTareasPendientes = crearListaVacia();
-    Nodo *listaTareasRealizadas = crearListaVacia();
-    pedirTarea(&listaTareasPendientes);
-    liberarLista(listaTareasPendientes);
+    
+    Nodo *startTareasPend = crearListaVacia();
+    Nodo *startTareasRealiz = crearListaVacia();
+    int opcion;
+    pedirTarea(&startTareasPend);
+    printf("\n¿Qué operación desea realizar?\n");
+    printf("1- Ingresar nueva tarea pendiente\n");
+    printf("2- Mover tarea pendiente a realizadas\n");
+    printf("3- Mostrar listas\n");
+    scanf("%d", &opcion);
+    do{
+        switch (opcion)
+        {
+    case 1:
+        pedirTarea(&startTareasPend);
+        break;
+    case 2:
+        int idBuscar;    
+        do{
+            printf("Ingrese el id de la tarea a transferir (id mayor que 1000): \n");
+            scanf("%d", &idBuscar);
+            if(buscarNodo(&startTareasPend, idBuscar, &startTareasRealiz)){
+                eliminarNodo(&startTareasPend, idBuscar);
+            }else{
+                break;
+            }
+        }while(idBuscar < 1000);
+        break;
+    case 3:
+        printf("Tareas pendientes: ");
+        printf("\n");
+        mostrarLista(&startTareasPend);
+        printf("\n");
+        printf("Tareas realizadas: ");
+        printf("\n");
+        mostrarLista(&startTareasRealiz);
+        break;
+    
+    default:
+        break;
+        }
+    }while(opcion == 0);
+    
+    // Nodo *listaTareasRealizadas = crearNodo();
+    
+    
+    liberarLista(startTareasRealiz);
+    liberarLista(startTareasPend);
     return 0;
 
 
@@ -43,7 +93,7 @@ Nodo * crearNodo(Tarea nuevaTarea)
 }
 
 
-void pedirTarea(Nodo **lista) {
+void pedirTarea(Nodo **start) {
     int respuesta;
     static int i = 1000; 
     
@@ -66,21 +116,21 @@ void pedirTarea(Nodo **lista) {
         
         nuevaTarea.TareaID = i++;
         
-        insertarNodo(lista, nuevaTarea);        
+        insertarNodoAlFinal(start, nuevaTarea);        
         printf("Si quiere agregar otra tarea (1: Sí, 0: No): ");
         scanf("%d", &respuesta);
         
     } while (respuesta != 0);
 }
 
-void insertarNodoAlFinal (Nodo **lista, Tarea nuevaTarea)
+void insertarNodoAlFinal (Nodo **start, Tarea nuevaTarea)
 {
     Nodo *nuevoNodo = crearNodo(nuevaTarea);
     
-    if (*lista == NULL) {
-        *lista = nuevoNodo;
+    if (*start == NULL) {
+        *start = nuevoNodo;
     } else {
-        Nodo *actual = *lista;
+        Nodo *actual = *start;
         while (actual->Siguiente) {
             actual = actual->Siguiente;
         }
@@ -88,8 +138,8 @@ void insertarNodoAlFinal (Nodo **lista, Tarea nuevaTarea)
     }
 }
 
-void liberarLista(Nodo *lista) {
-    Nodo *actual = lista;
+void liberarLista(Nodo *start) {
+    Nodo *actual = start;
     while (actual != NULL) {
         Nodo *temp = actual;
         actual = actual->Siguiente;
@@ -98,10 +148,49 @@ void liberarLista(Nodo *lista) {
     }
 }
 
+int buscarNodo(Nodo **start, int id, Nodo **startListaNueva){
+    Nodo * Aux = *start;
+    while(Aux && Aux->T.TareaID != id)
+    {
+    Aux = Aux -> Siguiente;
+    }
+    if(Aux){
+        Tarea tareaTrans = Aux->T;
+        insertarNodoAlFinal(startListaNueva, tareaTrans);
+        return 1;
+    }else{
+        printf("Id no encontrado");
+        return 0;
+    }
+    
+}
 
+void eliminarNodo (Nodo **start, int id){
+    Nodo *nodoAux = (*start);
+    Nodo *nodoAnt = NULL;
+    while (nodoAux != NULL && nodoAux->T.TareaID != id){
+        nodoAnt = nodoAux;
+        nodoAux = nodoAux->Siguiente;
+    }
+    if (nodoAux != NULL){
+        if (nodoAux == (*start)){
+            (*start) = nodoAux->Siguiente;
+        }
+        else{
+        nodoAnt->Siguiente = nodoAux->Siguiente;
+        }
+        nodoAux->Siguiente = NULL;
+        free(nodoAux->T.Descripcion);
+        free(nodoAux);
+        }
+    }
 
-
-
-
-
-
+    void mostrarLista(Nodo **start){
+        Nodo *nodoAux = (*start);
+        while(nodoAux != NULL){
+            printf("Id: %d\n", nodoAux->T.TareaID);
+            printf("Descripcion: %s\n", nodoAux->T.Descripcion);
+            printf("Duracion: %d\n\n", nodoAux->T.Duracion);
+            nodoAux = nodoAux->Siguiente;
+        }
+    }
